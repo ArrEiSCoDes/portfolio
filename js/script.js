6866620a9ceb96868ac000c4774f700fe9ea58c7
Sierra covington <sierrainc96@outlook.com>
2026-06-30 10:48:26 -0400

    Add JavaScript functionality

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Welcome Message
+console.log("Welcome to Sierra's Portfolio!");
+
+// Button Message
+function welcomeMessage() {
+    alert("Thank you for visiting my portfolio! I'm currently learning HTML, CSS, JavaScript, Git, AI, and Cybersecurity.");
+}
