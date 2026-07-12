2013de5bf2a65edf188ffa914a4ae173a7e90434
Sierra covington <sierrainc96@outlook.com>
2026-07-11 22:19:18 -0400

    Add theme toggle functionality to script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,24 @@ console.log("Welcome to Sierra's Portfolio!");
 function welcomeMessage() {
     alert("Thank you for visiting my portfolio! I'm currently learning HTML, CSS, JavaScript, Git, AI, and Cybersecurity.");
 }
+// ==========================
+// ORCHID TECH THEME SWITCH
+// ==========================
+
+
+const themeButton =
+document.getElementById("themeToggle");
+
+
+if(themeButton){
+
+
+themeButton.addEventListener("click",()=>{
+
+
+document.body.classList.toggle("dark-mode");
+
+
+});
+
+}
